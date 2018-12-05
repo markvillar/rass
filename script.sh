dfcc4f0008c5f75db504fac23e541f779696918e
Mark Villar <markcvillar@gmail.com>
2018-12-05 15:23:18 +0000

    some important (possibly useful) nmap flags

--- script.sh
+++ script.sh
@@ -3,3 +3,10 @@
 # 192.168.3.0/24
 # TCP Ports 21, 22, 23, 53, 79, 80, 123
 #
+# -sS SYN scan
+# -Pn Treat all hosts as online -- skip host discovery
+# -n/-R Never do DNS resolution/Always resolve [default: sometimes]
+# -sS TCP SYN/Connect()/ACK/Window/Maimon scans
+# --min-parallelism/max-parallelism <numprobes>: Probe parallelization
+# -vV Double Verbose
+# 
