48f05cff99eff8d5a03caa229858a6a645abbb88
Mark Villar <markcvillar@gmail.com>
2018-12-07 12:42:56 +0000

    Potential Solution

--- script.sh
+++ script.sh
@@ -10,3 +10,28 @@
 # --min-parallelism/max-parallelism <numprobes>: Probe parallelization
 # -vV Double Verbose
 # 
+# --scan-delay/--max-scan-delay <time>: Adjust delay between probes
+
+declare -a ports=(21 22 23 53 79 80 123)
+
+# nmap -p [21,22,23,53,79,80,123] 45.33.32.156 --scan-delay 500
+# nmap -sS -p [21,22,23,53,79,80,123] 45.33.32.156/32
+
+
+for i in {0..255}; do
+
+    for j in ${ports[@]}; do
+
+        nmap -v -sS -Pn -n -p $j 45.33.32.$i
+
+        sleep 200ms
+
+        nmap -v -sS -Pn -n -p $j 45.33.32.$i
+
+        sleep 300ms
+
+    done
+
+done
+
+# nmap -v -sS -Pn -n -p [21,22,23,53,79,80,123] 45.33.32.156
