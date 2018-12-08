ef67375b658e8fbe5200aab774d31750e1c689d4
Mark Villar <markcvillar@gmail.com>
2018-12-08 23:05:19 +0000

    Event & Odd Established

--- script.sh
+++ script.sh
@@ -15,28 +15,43 @@
 # nmap -p [21,22,23,53,79,80,123] 45.33.32.156 --scan-delay 500
 # nmap -sS -p [21,22,23,53,79,80,123] 45.33.32.156/32
 
-declare -a ports=(21, 22, 23, 53, 79, 80, 123)
+declare -a ports=(21, 22, 23)
+declare -a ipaddress=($(seq 0 1 255))
 
 # Define Even IP Addresses
 evens=($(seq 0 2 255))
 # Define Odd IP Addresses
 odds=($(seq 1 2 255))
 
-for port in ${ports[@]}; do
+for ip in ${ipaddress[@]}; do
 
-    for odd in ${odds[@]}; do
-        
-        nmap -v -sS -p $port 45.33.32.$odd
+    if [ $((ip%2)) -eq 0 ];
+    then
 
-    done
+        echo "$ip Even";
 
-    for even in ${evens[@]}; do
+        # nmap -v -sS -Pn -n -p $port 45.33.32.$ip
 
-        nmap -v -sS -p $port 45.33.32.$even
-        
-    done
+    else
+
+        echo "$ip Odd";
+
+        # nmap -v -sS -Pn -n -p $port 45.33.32.$ip
+
+    fi
 
 done
+
+
+# for port in ${ports[@]}; do
+
+#     for i in {0..3}; do
+
+#         echo $port
+
+#     done
+
+# done
 
 
 # for i in {0..255}; do
