126f33cce02891918b8c46d150abf4727fcffbae
Mark Villar <markcvillar@gmail.com>
2018-12-09 22:01:55 +0000

    use Space!!!

--- script.sh
+++ script.sh
@@ -16,7 +16,7 @@
 # nmap -sS -p [21,22,23,53,79,80,123] 45.33.32.156/32
 # echo $BASH_VERSION
 
-declare -a ports=(21, 22, 23, 53, 79, 80, 123)
+declare -a ports=(21 22 23 53 79 80 123)
 declare -a ipaddress=($(seq 0 1 1))
 
 declare -a ipScanned=()
